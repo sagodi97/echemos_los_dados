b8a04396a9f781ee5622bc55b8c27b6d58d8fa36
Santiago Gonzalez <sagodi97@gmail.com>
2019-07-15 07:01:02 +0200

    Add icons and update to use stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,80 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.blueGrey[900],
       appBar: AppBar(
-        title: Text('Echemos Los Dados'),
-        backgroundColor: Colors.orange,
+        title: Text(
+          'Echemos Los Dados',
+          style: TextStyle(color: Colors.blueGrey[900]),
+        ),
+        backgroundColor: Colors.grey[50],
       ),
       body: RollTheDicePage(),
     ),
   ));
 }
 
-class RollTheDicePage extends StatelessWidget {
+class RollTheDicePage extends StatefulWidget {
+  @override
+  _RollTheDicePageState createState() => _RollTheDicePageState();
+}
+
+class _RollTheDicePageState extends State<RollTheDicePage> {
+  int daditoR = 2;
+  int daditoL = 3;
+  randomea() {
+    setState(() {
+      daditoL = Random().nextInt(6) + 1;
+      daditoR = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var dadito = 2;
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                dadito = 3;
-              },
-              child: Image.asset('images/dice$dadito.png'),
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    randomea();
+                  },
+                  child: Image.asset('images/dice$daditoL.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    randomea();
+                  },
+                  child: Image.asset('images/dice$daditoR.png'),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: FlatButton(
+          SizedBox(
+            height: 100,
+          ),
+          ButtonTheme(
+            minWidth: 120,
+            height: 120,
+            child: RaisedButton(
               onPressed: () {
-                //TODO
+                randomea();
               },
-              child: Image.asset('images/dice$dadito.png'),
+              shape: CircleBorder(),
+              color: Colors.grey[50],
+              textTheme: ButtonTextTheme.normal,
+              child: Text(
+                "Echalos!",
+                style: TextStyle(fontSize: 20),
+              ),
             ),
           ),
         ],
